530d61bfa8a481dde4605d5b246dc0b08d13bcd8
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2025-01-29 17:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,22 @@ getSumBtn.append("Get Total Price");
 document.body.appendChild(getSumBtn);
 
 const getSum = () => {
-//Add your code here
-  
+	let priceTd = document.getElementsByClassName("price");
+	let sum = 0;
+	Array.from(priceTd).forEach(item => {
+		sum += parseInt(item.innerText) || 0;
+	})
+  let sumRow = document.createElement("tr");
+	sumRow.innerHTML = `
+		<td id="total">Total</td>
+		<td id="sum">${sum}</td>
+	`;
+	let table = document.getElementsByTagName("table")[0];
+	if(table){
+		table.appendChild(sumRow);
+	} else {
+		console.log("No table exist.");
+	}
 };
 
 getSumBtn.addEventListener("click", getSum);
